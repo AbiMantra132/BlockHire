3a1d3c2d1c22244b0cf14e781bf68f27b2892c80
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2025-02-17 14:27:11 +0800

    SET UP ICP

---
+++ src/BlockHire_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { BlockHire_backend } from 'declarations/BlockHire_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    BlockHire_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
