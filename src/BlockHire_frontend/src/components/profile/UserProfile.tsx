f0bd491ecd872f31d8a048770a50de06edfecd86
GungWira <wiradarma002@gmail.com>
2025-02-28 22:26:12 +0800

    Update profile

--- src/BlockHire_frontend/src/components/profile/UserProfile.tsx
+++ src/BlockHire_frontend/src/components/profile/UserProfile.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import ProfileImage from "./ProfileImage";
 import DisplayData from "./DisplayData";
 import { useAuth } from "../../hooks/AuthProvider";
 
 export default function UserProfile() {
-  const { user, freelancer } = useAuth();
+  const { freelancer } = useAuth();
   return (
     <div className="w-96 bg-white rounded-xl px-4 py-6 flex flex-col justify-start items-start gap-4">
       {/* PROFILE */}
